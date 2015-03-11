cccff39062a8986722a88da81d9edb7f83df51d9
Kyle Brown <kdb424@gmail.com>
2015-03-11 18:12:26 -0400

    Hives on mars and mood generate, bees are starting to be added. Capsules were removed due to a massive update going to be needed, and frames added. Other general cleanup also.

--- src/main/java/spacebees/item/ItemCapsule.java
+++ src/main/java/spacebees/item/ItemCapsule.java
@@ -1,19 +1,22 @@
-package spacebees.item;
+//TODO Update to forestry standards
+/*package spacebees.item;
 
 import java.util.List;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import spacebees.item.types.CapsuleType;
-import spacebees.item.types.FluidType;
-import spacebees.main.CommonProxy;
-import spacebees.main.utils.TabSpaceBees;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import spacebees.item.types.CapsuleType;
+import spacebees.item.types.FluidType;
+import spacebees.main.CommonProxy;
+import spacebees.main.utils.TabSpaceBees;
+import spacebees.main.utils.TextureManager;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 
 public class ItemCapsule extends Item
 {
@@ -27,7 +30,7 @@ public class ItemCapsule extends Item
 		this.setHasSubtypes(true);
 		this.setMaxStackSize(maxStackSize);
 		this.setUnlocalizedName("capsule." + type.toString().toLowerCase());
-		GameRegistry.registerItem(this, "capsule." + type.toString().toLowerCase());
+		
 	}
 	
 	public CapsuleType getType()
@@ -66,35 +69,76 @@ public class ItemCapsule extends Item
 		return true;
 	}
 	
+	private IIcon[] icons;
+	
+	private int counter = 0;
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister par1IconRegister)
 	{
-		this.itemIcon = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":capsule" + this.capsuleType.getName().substring(0, 1).toUpperCase()
-				+ this.capsuleType.getName().substring(1));
+		System.out.println(par1IconRegister);
+		icons = new IIcon[2];
+		String capsuleName = "capsule"+ this.capsuleType.getName().substring(0, 1).toUpperCase()+ this.capsuleType.getName().substring(1);
+		icons[0] = TextureManager.getInstance().registerTex(par1IconRegister, capsuleName);
+//		icons[1] = TextureManager.getInstance().registerTex(par1IconRegister, capsuleName);
+		counter = 0;
+		int subcounter = 0;
+//		System.out.println(par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":capsule" + this.capsuleType.getName().substring(0, 1).toUpperCase()+ this.capsuleType.getName().substring(1)));
+//		icons[0] = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":capsule" + this.capsuleType.getName().substring(0, 1).toUpperCase()
+//				+ this.capsuleType.getName().substring(1));
+//		icons[1] = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":capsule" + this.capsuleType.getName().substring(0, 1).toUpperCase()
+//				+ this.capsuleType.getName().substring(1));
+//		this.icon = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":capsule" + this.capsuleType.getName().substring(0, 1).toUpperCase()
+//				+ this.capsuleType.getName().substring(1));
+		
 		for (FluidType t : FluidType.values())
-		{
-			if (t != FluidType.EMPTY && t.liquidIcon == null)
-			{
-				t.liquidIcon = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":liquids/" + t.liquidID.toLowerCase());
+		{			
+//			if (t != FluidType.EMPTY && this.icons[1] == null)
+//			{
+//				icons[1] = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":liquids/" + t.liquidID.toLowerCase());
+//			}
+			if(subcounter == counter && t != FluidType.EMPTY){
+				this.icons[1] = par1IconRegister.registerIcon(CommonProxy.DOMAIN + ":liquids/" + t.liquidID.toLowerCase());
+				System.out.println(this.icons[1]);
+			}
+			else{
+				++counter;
+				++subcounter;
 			}
 		}
+//		System.out.println(par1IconRegister);
+//		System.out.println("Registered " + counter + " icons");
+//		System.out.println("Registered " + subcounter + " icons");
 	}
 
-	@Override
+//	@Override
+//	@SideOnly(Side.CLIENT)
+//	public IIcon getIconFromDamageForRenderPass(int metadata, int pass)
+//	{
+//		System.out.println("1");
+//		IIcon i = this.itemIcon;
+//		if (metadata != 0 && pass == 0)
+//		{
+//			System.out.println(i);
+//			i = FluidType.values()[Math.max(0, Math.min(metadata, FluidType.values().length - 1))].liquidIcon;
+//			System.out.println(i);
+//		}
+//		return i;
+//	}
 	@SideOnly(Side.CLIENT)
-	public IIcon getIconFromDamageForRenderPass(int metadata, int pass)
-	{
-		IIcon i = this.itemIcon;
-		if (metadata != 0 && pass == 0)
-		{
-			i = FluidType.values()[Math.max(0, Math.min(metadata, FluidType.values().length - 1))].liquidIcon;
+	@Override
+	public IIcon getIconFromDamageForRenderPass(int i, int j) {
+		if (j > 0 ) {
+			return icons[1];
+		} else {
+			return icons[0];
 		}
-		return i;
 	}
 
 	@Override
 	public int getRenderPasses(int metadata)
 	{
-		return (metadata == 0) ? 1 : 2;
+//		return (metadata == 0) ? 1 : 2;
+		return 2;
 	}
 }
+*/
